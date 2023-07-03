a0b3d16f889fca3fc60e9fde95ceabc04b78fa47
artemiusV <artemius.visotski@gmail.com>
2023-07-03 10:01:47 +0300

    add component pizzaBlock

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import React from "react";
+import "./scss/app.scss";
+
 import Header from "./components/Header";
 import Categories from "./components/Categories";
 import Sort from "./components/Sort";
 import PizzaBlock from "./components/PizzaBlock";
+import pizzas from "./assets/pizzas.json";
 
-import "./scss/app.scss";
+console.log(pizzas);
 
 function App() {
   return (
-    <div class="wrapper">
+    <div className="wrapper">
       <Header />
-      <div class="content">
-        <div class="container">
-          <div class="content__top">
+      <div className="content">
+        <div className="container">
+          <div className="content__top">
             <Categories />
             <Sort />
           </div>
-          <h2 class="content__title">Все пиццы</h2>
-          <div class="content__items">
-            <PizzaBlock title="Мексиканская" price={32} />
-            <PizzaBlock title="Гавайская" price={35} />
+          <h2 className="content__title">Все пиццы</h2>
+          <div className="content__items">
+            {pizzas.map((obj) => (
+              <PizzaBlock
+                title={obj.title}
+                price={obj.price}
+                imageUrl={obj.imageUrl}
+                sizes={obj.sizes}
+                types={obj.types}
+              />
+            ))}
           </div>
         </div>
       </div>
