b7a055d753053869f159b2980d539e49779906f6
artemiusV <artemius.visotski@gmail.com>
2023-07-08 19:53:36 +0300

    added TypeScript

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
@@ -6,12 +5,17 @@ import { Provider } from "react-redux";
 
 import { store } from "./redux/store";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const rootElem= document.getElementById("root")
 
-root.render(
-  <Provider store={store}>
+if(rootElem){
+  const root = ReactDOM.createRoot(rootElem);
+
+  root.render(
     <BrowserRouter>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </BrowserRouter>
-  </Provider>
 );
+}
+
